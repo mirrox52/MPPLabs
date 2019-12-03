dc4e2cbc53ea22bc9973e1fb04dc1fa094fed625
mirrox52 <47641217+mirrox52@users.noreply.github.com>
2019-12-03 20:12:12 +0300

    Add files via upload

---
+++ laba_1/laba_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue _TaskQueue = null;
+            try
+            {
+                Console.Write("Enter the number of threads: ");
+                int countThreads = int.Parse(Console.ReadLine());
+                _TaskQueue = new TaskQueue(countThreads);
+
+                for (int i = 0; i < countThreads*10; i++)
+                {
+                    _TaskQueue.EnqueueTask(SomeTask);
+                }
+                _TaskQueue.Wait();
+            }
+            finally
+            {
+                if(_TaskQueue != null)
+                {         
+                   _TaskQueue.Dispose();
+                }
+            }
+                                    
+        }
+
+        static public void SomeTask()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name);
+        }
+        
+    }
+}
---
+++ laba_1/laba_1/TaskQueue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace laba_1
+{
+    class TaskQueue : IDisposable
+    {
+        public delegate void TaskDelegate();        
+        private ConcurrentQueue<TaskDelegate> tasks;                // очередь
+        private List<Thread> threads;
+
+        public TaskQueue(int count)                                 // конструктор
+        {
+            tasks = new ConcurrentQueue<TaskDelegate>();            // инициализация очереди
+            threads = new List<Thread>();                           // создание списка потоков (пул)
+
+            Thread thread;
+            for (int i = 0; i < count; i++)                         // пул потоков
+            {
+                thread = new Thread(new ThreadStart(Work));
+                thread.Name = "Thread " + i.ToString();
+                threads.Add(thread);
+                threads[i].Start();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            tasks.Enqueue(task);                                    // добавляет элемент в конец очереди
+        }
+
+        private void Work()
+        {   
+            while(true)                                           
+            {
+                try
+                {
+                    TaskDelegate task;
+                    while (!tasks.TryDequeue(out task))             // если нет доступа к таске поток ожидает
+                       Thread.Sleep(10);
+                    task();
+                }
+                catch (Exception exc)
+                {
+                   
+                }
+            }         
+        }
+
+        public void Wait()
+        {
+            foreach (Thread thread in threads)
+            {
+                thread.Join();                                      
+            }
+        }
+
+        public void CloseThreads()
+        {
+            foreach (Thread thread in threads)
+            {
+                thread.Interrupt();
+            }
+        }
+
+        public void Dispose()
+        {
+            CloseThreads();
+            GC.SuppressFinalize(this);
+        }
+
+        ~TaskQueue()
+        {
+            CloseThreads();
+        }
+
+    }
+}
